6ce1246d8bd8d6d10732aceb40ad980a36a9d9e5
Viv-Iam <vivian.opondoh@gmail.com>
2017-02-24 10:43:35 +0300

    added all files in correct format

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+for (var i = 1; i <= parseInt(input); i++ ) {
+  if (i % 3 === 0) {
+    if (i % 5 === 0) {
+      console.log("pingpong");
+    } else {
+      console.log("ping");
+    }
+  } else if (i % 5 === 0) {
+    console.log("pong");
+  } else {
+    console.log(i);
+  }
+}
